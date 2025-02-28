73258267f44fceaa0b60ebeb899d211d1e6b1f72
Blockchain-Oracle <abubakrjimoh16488@gmail.com>
2025-02-28 22:32:50 +0100

    Update gitignore and remove sensitive files

--- .next/server/app/api/token/metadata/solana/[...cid]/route_client-reference-manifest.js
+++
@@ -1,2 +0,0 @@
-globalThis.__RSC_MANIFEST = globalThis.__RSC_MANIFEST || {};
-globalThis.__RSC_MANIFEST["/api/token/metadata/solana/[...cid]/route"] = {"moduleLoading":{"prefix":"/_next/","crossOrigin":null},"clientModules":{},"ssrModuleMapping":{},"edgeSSRModuleMapping":{},"rscModuleMapping":{},"edgeRscModuleMapping":{},"entryCSSFiles":{},"entryJSFiles":{}}
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"AwyvwispnO6KV0GQAO1Hdh7FbzyYjXsSPAq2/PoDb6c=\"\n}"
